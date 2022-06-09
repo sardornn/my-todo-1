2d023b9eb848ae224d7b80079baffb8f94188338
sardornn <sardornoraliev01@gmail.com>
2022-06-10 03:35:12 +0500

    refactor some codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+import Todo from './components/Todo/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = React.useState([]);
+
+	const handleDelete = (evt) => {
+		const todoId = evt.target.dataset.todoId - 0;
+
+		const filteredTodos = todos.filter((todo) => todo.id !== todoId);
+
+		setTodos(filteredTodos);
+	};
+
+	const handleCheck = (evt) => {
+		const todoId = evt.target.dataset.todoId - 0;
+
+		const foundTodo = todos.find((todo) => todo.id === todoId);
+
+		foundTodo.isCompleted = !foundTodo.isCompleted;
+
+		// setTodos(todos);
+		setTodos([...todos]);
+	};
+
+	return (
+		<main className='main'>
+			<input
+				type='text'
+				onKeyUp={(evt) => {
+					if (evt.code === 'Enter') {
+						const newTodo = {
+							id: todos[todos.length - 1]?.id + 1 || 0,
+							title: evt.target.value.trim(),
+							isCompleted: false,
+						};
+
+						setTodos([...todos, newTodo]);
+
+						evt.target.value = null;
+					}
+				}}
+			/>
+
+			<ul className='todos'>
+				{todos.length > 0 &&
+					todos.map((todo) => (
+						<Todo key={todo.id} todo={todo} handleDelete={handleDelete} handleCheck={handleCheck}>
+							{todo.title}
+						</Todo>
+					))}
+			</ul>
+		</main>
+	);
 }
 
 export default App;
+
+
